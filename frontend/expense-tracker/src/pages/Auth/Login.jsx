4e19eb435ed3a641aec0274b331975d765fa78be
KARISHMA ACHARYA <134969120+acharyakarishma31@users.noreply.github.com>
2025-11-04 04:24:56 +0530

    Update Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -5,7 +5,7 @@ import {Link, useNavigate} from "react-router-dom";
 import {validateEmail} from "../../utils/helper";
 import {API_PATHS} from "../../utils/apiPaths";
 import axiosInstance from '../../utils/axiosInstance';
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../../context/UserContext';
 
 const Login = () => {
   const [email, setEmail]=useState("");
